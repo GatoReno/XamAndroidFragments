bb1f9a0e78dac19838faea0d1174e031fc7ccd4f
Ed <pushpoped@gmail.com>
2021-10-27 23:31:49 -0400

    first commit

---
+++ FragPractice/Activities/PlayQuoteActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using AndroidX.Fragment.App;
+using FragPractice.Fragments;
+
+namespace FragPractice.Activities
+{
+    [Activity(Label = "PlayQuoteActivity")]
+    public class PlayQuoteActivity : AndroidX.Fragment.App.FragmentActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var playId = Intent.Extras.GetInt("current_play_id", 0);
+            var detailsFrag = PlayQuoteFragment.NewInstance(playId);
+            SupportFragmentManager.BeginTransaction()
+                            .Add(
+                                Android.Resource.Id.Content, detailsFrag)
+                            .Commit();
+        }
+    }
+}
---
+++ FragPractice/obj/Debug/android/src/crc646e3314ea2bc4e0e1/TitlesFragment.java
@@ -0,0 +1,74 @@
+package crc646e3314ea2bc4e0e1;
+
+
+public class TitlesFragment
+	extends androidx.fragment.app.ListFragment
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onCreateView:(Landroid/view/LayoutInflater;Landroid/view/ViewGroup;Landroid/os/Bundle;)Landroid/view/View;:GetOnCreateView_Landroid_view_LayoutInflater_Landroid_view_ViewGroup_Landroid_os_Bundle_Handler\n" +
+			"n_onSaveInstanceState:(Landroid/os/Bundle;)V:GetOnSaveInstanceState_Landroid_os_Bundle_Handler\n" +
+			"n_onListItemClick:(Landroid/widget/ListView;Landroid/view/View;IJ)V:GetOnListItemClick_Landroid_widget_ListView_Landroid_view_View_IJHandler\n" +
+			"";
+		mono.android.Runtime.register ("FragPractice.Fragments.TitlesFragment, FragPractice", TitlesFragment.class, __md_methods);
+	}
+
+
+	public TitlesFragment ()
+	{
+		super ();
+		if (getClass () == TitlesFragment.class)
+			mono.android.TypeManager.Activate ("FragPractice.Fragments.TitlesFragment, FragPractice", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public android.view.View onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2)
+	{
+		return n_onCreateView (p0, p1, p2);
+	}
+
+	private native android.view.View n_onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2);
+
+
+	public void onSaveInstanceState (android.os.Bundle p0)
+	{
+		n_onSaveInstanceState (p0);
+	}
+
+	private native void n_onSaveInstanceState (android.os.Bundle p0);
+
+
+	public void onListItemClick (android.widget.ListView p0, android.view.View p1, int p2, long p3)
+	{
+		n_onListItemClick (p0, p1, p2, p3);
+	}
+
+	private native void n_onListItemClick (android.widget.ListView p0, android.view.View p1, int p2, long p3);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
